3e19b86b189aa769f134194ab52e12ac7e598b1b
Leaxx <theleaxx01@gmail.com>
2023-10-14 01:15:14 +0300

    Added GitHub link

--- LaikaAddons/LaikaAddons.cs
+++ LaikaAddons/LaikaAddons.cs
@@ -13,7 +13,7 @@ namespace LaikaAddons
         public override string ModAuthor => "Leaxx"; // The mod's author (you). Also shown in the mods list.
         public override string ModDescription => "Adds various features to the Laika for more immersive gameplay!"; // The mod's description. This is also shown in the mods list, upon clicking on "More Info".
         public override string ModVersion => "1.0.0"; // The mod's version. Also shown in the mods list. If your mod is open-source on GitHub, make sure that you're using the same format as your release tags (for example, 1.0.0)
-        public override string GitHubLink => ""; // If your mod is open-source on GitHub, you can link it here to allow for automatic update-checking in-game. It compares the current ModVersion with the tag of the latest release (ex. 1.0.0 compared with 1.0.1)
+        public override string GitHubLink => "https://github.com/Jalopy-Mods/LaikaAddons"; // If your mod is open-source on GitHub, you can link it here to allow for automatic update-checking in-game. It compares the current ModVersion with the tag of the latest release (ex. 1.0.0 compared with 1.0.1)
         public override WhenToInit WhenToInit => WhenToInit.InGame; // When should the mod's OnEnable/Awake/Start/Update functions be called?
 
         public override bool UseAssets => true; // Does your mod use custom assetbundles?
